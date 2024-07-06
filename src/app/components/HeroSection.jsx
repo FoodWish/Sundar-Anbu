8379bdbc5e678c8a3bf65e330eb7547b1f3de548
Sundar Anbu <sundar@ken42.com>
2024-07-06 17:18:18 +0530

    data

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,13 +37,15 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://www.linkedin.com/in/sundar-anbu-a94614202/"
+              target="_blank"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Contact
             </Link>
             <Link
-              href="/"
+            target="_blank"
+              href="https://s3.ap-south-1.amazonaws.com/insdms2.0/'/s3/buckets/Sundar+Anbu++.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
